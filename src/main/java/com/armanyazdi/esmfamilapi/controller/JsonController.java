695f9eef3b6af409fb83c3cbc797a847dc84272e
Arman Yazdi <f41c0n.32@gmail.com>
2023-04-18 02:18:24 +0330

    Update JsonController.java

--- src/main/java/com/armanyazdi/esmfamilapi/controller/JsonController.java
+++ src/main/java/com/armanyazdi/esmfamilapi/controller/JsonController.java
@@ -1,14 +1,14 @@
 package com.armanyazdi.esmfamilapi.controller;
 
 import com.armanyazdi.esmfamilapi.response.EsmFamil;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class JsonController {
     @GetMapping("/esmfamil")
-    public EsmFamil json(@RequestParam char letter) {
+    @ResponseBody
+    public EsmFamil jsonResponse(@RequestParam char letter) {
         return new EsmFamil(letter);
     }
 }
