8f3171c1d0d57963836ac383e5bb13a55e356a93
Arman Yazdi <f41c0n.32@gmail.com>
2023-04-17 11:57:35 +0330

    Update JsonController.java

--- src/main/java/com/armanyazdi/esmfamilapi/controller/JsonController.java
+++ src/main/java/com/armanyazdi/esmfamilapi/controller/JsonController.java
@@ -8,30 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JsonController {
     @GetMapping("/esmfamil")
-    public EsmFamil json(@RequestParam char letter,
-                         @RequestParam(required = false) String name,
-                         @RequestParam(required = false) String family,
-                         @RequestParam(required = false) String food,
-                         @RequestParam(required = false) String fruit,
-                         @RequestParam(required = false) String color,
-                         @RequestParam(required = false) String city,
-                         @RequestParam(required = false) String country,
-                         @RequestParam(required = false) String animal,
-                         @RequestParam(required = false) String flower,
-                         @RequestParam(required = false) String car,
-                         @RequestParam(required = false) String object) {
-        return new EsmFamil(
-                letter,
-                name,
-                family,
-                food,
-                fruit,
-                color,
-                city,
-                country,
-                animal,
-                flower,
-                car,
-                object);
+    public EsmFamil json(@RequestParam char letter) {
+        return new EsmFamil(letter);
     }
 }
