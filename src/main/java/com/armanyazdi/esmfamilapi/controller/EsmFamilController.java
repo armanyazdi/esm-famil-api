8917f327c2bd309554ce1ccac1e783e6c15008df
Arman Yazdi <f41c0n.32@gmail.com>
2023-04-19 03:32:36 +0330

    Update EsmFamilController.java

--- src/main/java/com/armanyazdi/esmfamilapi/controller/EsmFamilController.java
+++ src/main/java/com/armanyazdi/esmfamilapi/controller/EsmFamilController.java
@@ -1,6 +1,7 @@
 package com.armanyazdi.esmfamilapi.controller;
 
-import com.armanyazdi.esmfamilapi.response.EsmFamil;
+import com.armanyazdi.esmfamilapi.EsmFamil;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +11,12 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/api")
-public class JsonController {
-    private final AtomicLong counter = new AtomicLong();
+public class EsmFamilController {
+    private static final AtomicLong counter = new AtomicLong();
+    private static final HttpStatus status = HttpStatus.OK;
 
     @GetMapping("/esmfamil")
-    public EsmFamil jsonResponse(@RequestParam char letter) {
-        return new EsmFamil(counter.incrementAndGet(), letter);
+    public EsmFamil esmfamil(@RequestParam char letter) {
+        return new EsmFamil(counter.incrementAndGet(), status, letter);
     }
 }
