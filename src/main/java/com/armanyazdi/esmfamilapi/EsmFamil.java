297f1e66ab3a8a9eb0be18935c564f1c7f74f399
Arman Yazdi <f41c0n.32@gmail.com>
2023-04-27 17:02:47 +0330

    Update EsmFamil.java

--- src/main/java/com/armanyazdi/esmfamilapi/EsmFamil.java
+++ src/main/java/com/armanyazdi/esmfamilapi/EsmFamil.java
@@ -8,17 +8,7 @@ public class EsmFamil {
     private final Long id;
     private final HttpStatus status;
     private final char letter;
-    private final String name;
-    private final String family;
-    private final String food;
-    private final String fruit;
-    private final String color;
-    private final String city;
-    private final String country;
-    private final String animal;
-    private final String flower;
-    private final String car;
-    private final String object;
+    private final String name, family, food, fruit, color, city, country, animal, flower, car, object;
 
     public EsmFamil(Long id, HttpStatus status, char letter) {
         this.id = id;
@@ -26,14 +16,14 @@ public class EsmFamil {
         this.letter = letter;
         this.name = NameGenerator.firstName(letter);
         this.family = NameGenerator.lastName(letter);
-        this.food = RandomSelector.select(letter, "foods.txt");
-        this.fruit = RandomSelector.select(letter, "fruits.txt");
-        this.color = RandomSelector.select(letter, "colors.txt");
-        this.city = RandomSelector.select(letter, "cities.txt");
-        this.country = RandomSelector.select(letter, "countries.txt");
-        this.animal = RandomSelector.select(letter, "animals.txt");
-        this.flower = RandomSelector.select(letter, "flowers.txt");
-        this.car = RandomSelector.select(letter, "cars.txt");
-        this.object = RandomSelector.select(letter, "objects.txt");
+        this.food = RandomSelector.selectItem(letter, "foods.txt");
+        this.fruit = RandomSelector.selectItem(letter, "fruits.txt");
+        this.color = RandomSelector.selectItem(letter, "colors.txt");
+        this.city = RandomSelector.selectItem(letter, "cities.txt");
+        this.country = RandomSelector.selectItem(letter, "countries.txt");
+        this.animal = RandomSelector.selectItem(letter, "animals.txt");
+        this.flower = RandomSelector.selectItem(letter, "flowers.txt");
+        this.car = RandomSelector.selectItem(letter, "cars.txt");
+        this.object = RandomSelector.selectItem(letter, "objects.txt");
     }
 }
